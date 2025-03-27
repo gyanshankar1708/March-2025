aa96c94d9ee2f18e4fc0bca83c23327ace2cde58
Gyanshankar <gyanshankar1708@gmail.com>
2025-03-27 15:26:52 +0530

    27th March

---
+++ 27th March/script.js
@@ -0,0 +1,36 @@
+let con = document.getElementById("container");
+const html = (val) => {
+  ans = `<div class="container">
+        <div class="text">
+            $ : ${val}
+        </div>
+        <div class="dots">...</div>
+    </div>`;
+  return ans;
+};
+let arr = [
+  "Initializing Hacking",
+  "Reading your files",
+  "Password file detected",
+  "Sending all password files and personal information to server",
+  "Cleaning up",
+];
+
+
+async function display(val){
+    let randNum = Math.random()*7;
+    var prom = new Promise((ressolve,reject)=>{
+        setTimeout(()=>{
+            con.innerHTML = con.innerHTML+html(val);
+            ressolve("Done")
+        },randNum*1000)
+    })
+    return prom;
+}
+async function main(){
+    for (const msg of arr){
+        await display(msg);
+        console.log(msg);
+    }
+}
+main();
